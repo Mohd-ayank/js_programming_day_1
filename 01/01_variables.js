509f6dd049f9a75861f8887f0ce6d5d8d2a21d93
Mohd-ayank <100429377+Mohd-ayank@users.noreply.github.com>
2025-05-01 06:06:36 +0000

    data types and variables

---
+++ 01/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=144553
+//difference bwtween var and let
+//but use only "let" not "var"
+//var has block scope and functional scope problem
+
+let accountPass="12033303"
+let accountEmail="cusbwhd@gmail"
+let accountState;//undefined
+
+ 
+console.table([accountId,accountEmail,accountPass,accountState])
---
+++ 01/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict";//treat all JS code as newer version
+
+//number => 2 to power 53
+//bigint -> for very large number
+//null -> standalone value , special type of data type 
+//undefined -> not defined
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
