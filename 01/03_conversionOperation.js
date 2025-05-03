c8e3690c46eb7781f3151684e63b0d6e8516b3e8
Mohd-ayank <100429377+Mohd-ayank@users.noreply.github.com>
2025-05-03 03:04:39 +0000

    conversion of data types

---
+++ 01/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score="33";
+// console.log(typeof score);//string
+
+// //string to number
+
+// let valueInNumber=Number(score);
+// console.log(valueInNumber);//33
+
+// let score="33abc"
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber)//type-number
+// console.log(valueInNumber);//NaN (converted to an special type of number)
+
+//"33"=>33
+//"33abc"=>NaN
+
+////to boolean
+
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+//1=>true; 0=>false
+//"" =>false
+//"hitesh" =>true
+
+////to string
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)//string
