d70c2f6fe9a2c3aebdd5899f4bb6117e9648c7b1
Mohd-ayank <100429377+Mohd-ayank@users.noreply.github.com>
2025-05-03 03:32:06 +0000

    operators

---
+++ 01/04_operations.js
@@ -0,0 +1,21 @@
+// let value=3;
+// let negValue=-value;
+// console.log(negValue);//-3
+//consoloe.log(2+2)//4
+//console.log(2**4)//2 res to the power 4=16
+
+// let str1="hello"
+// let str2=" hitesh"
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32-  (1+2)->3+"2"->32
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+//prefix and postfix operators
---
+++ 01/05_comparison.js
@@ -0,0 +1,12 @@
+//>,>=,==,!=,===
+
+// console.log("2">1);
+// console.log("02">1);
+//Remember: whenver compare two values their data types must be same to get expected results
+
+//=== and ==
+console.log("2"==2);//true (no data type checking simply string is converted to int)
+console.log("2"===2);//false (data type is different)
+
+
+//conclusion : always compare same data types
