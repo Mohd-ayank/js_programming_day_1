f1a65b016f1cf6ae833059f761f3ef79f693284b
Mohd-ayank <100429377+Mohd-ayank@users.noreply.github.com>
2025-05-11 11:28:44 +0000

    number and math day after long break

---
+++ 01/09_numbers_maths.js
@@ -0,0 +1,23 @@
+// const score=400;//implicitely stored as number
+// console.log(score);
+
+
+// const balance=new Number(100);//specifically-> new object mai define kar raha hu jo number type ka hai
+// console.log(balance);
+
+// console.log(balance.toString().length);//become a string
+
+// console.log(balance.toFixed(2));//precision value upto 2
+
+const otherNumber=23.8966
+console.log(otherNumber.toPrecision(3));//
+//23.8966->23.9
+//123.8966->124
+//1123.8966->1.12e+3
+
+//Number.MIN_VALUE
+//Number.MAX_VALUE
+
+//+++++++++++++Maths+++++++++++++++++++++++
+
+//ceil, floor, trunc, round, min, max
