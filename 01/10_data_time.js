420575a7e1adad85696e95e4e8f639458a53f33d
Mohd-ayank <100429377+Mohd-ayank@users.noreply.github.com>
2025-05-12 03:18:24 +0000

    date and timestamp

---
+++ 01/10_data_time.js
@@ -0,0 +1,31 @@
+//Dates
+
+let myDate=new Date()//myDate is an object
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());//syntax are changed
+//and other methods
+
+// let myCreatedDate = new Date(2023,0,23,5,4)
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate=new Date("2023-01-04")
+//or
+
+
+//to compare two time stamps
+// let myCreatedDate=new Date("01-14-2023");
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));//in seconds
+
+let newDate=new Date()
+// console.log(newDate.getMonth());
+
+//interesting method
+// newDate.toLocaleString('default',{
+//     weekday:"long"
+//     timezone:""
+// })
