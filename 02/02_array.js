442cef987f523a60e1f2985100c3b4cc2273d747
Mohd-ayank <100429377+Mohd-ayank@users.noreply.github.com>
2025-05-16 04:25:09 +0000

    array method finished

--- 02/02_array.js
+++ 02/02_array.js
@@ -9,5 +9,29 @@ const dcHeros=["superman","flash","batman"]
 
 // console.log(marvelHeros[3][1]);//flash
 
-const allHeros=marvelHeros.concat(dcHeros);
-console.log(allHeros);//[ 'thor', 'ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+// const allHeros=marvelHeros.concat(dcHeros);
+// console.log(allHeros);//[ 'thor', 'ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+//spread (to merge multiple arrays)
+const all_new_heros=[...marvelHeros,...dcHeros];//... -> array spreading
+console.log(all_new_heros);
+
+//flat method
+const another_array=[1,2,3,[4,5,6],7,[4,5,[4,5,6]]];
+const real_another_array=another_array.flat(Infinity);//[1,2,3,4,5,6,7,4,5,4,5,6]
+
+//conversion from other datat type to array and checking if array or not
+console.log(Array.isArray("Hitesh"));//false
+console.log(Array.from("Hitesh"));//['H','i',......]
+console.log(Array.from({name:"hitesh"}));//[]->the thing which need to be converted is not specified clearly so returns empty array
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));//[ 100, 200, 300 ]
+
+
+
+
+
